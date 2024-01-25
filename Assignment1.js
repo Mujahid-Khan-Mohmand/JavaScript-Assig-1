419def1655b0e0dd6399373b4ee1605378d43e76
Mujahid-Khan-Mohmand <155892674+Mujahid-Khan-Mohmand@users.noreply.github.com>
2024-01-25 05:04:09 -0800

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,26 @@
+let Student="I am a Student";
+let age="18";
+
+console.log(Student);
+console.log('Age = ' +age);
+
+
+
+
+alert(' Imran Khan,\n \n"Khan was born into an affluent Pashtun family in Lahore and was educated at elite schools in Pakistan and the United Kingdom, including the Royal Grammar School in Worcester and Aitchison College in Lahore. There were several accomplished cricket players in his family, including two elder cousins, Javed Burki and Majid Khan, who both served as captains of the Pakistani national team. Imran Khan played cricket in Pakistan and the United Kingdom in his teens and continued playing while studying philosophy, politics, and economics at the University of Oxford. Khan played his first match for Pakistan’s national team in 1971."')
+
+
+let length = '23'
+let width = '20'
+
+let area = length*width;
+
+console.log('Area of Rectangle = ' +area);
+
+let nam = prompt('Enter Your Name:');
+alert(nam)
+
+console.log(nam)
+
+
+
